eebcb98f25aaf9599530fc4fc61510609efe086d
CarolFAraujo <127214777+CarolFAraujo@users.noreply.github.com>
2023-04-13 12:25:40 -0300

    Update PostagemRepository.java

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,7 +3,7 @@ package com.generation.blogpessoal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
-
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 }
